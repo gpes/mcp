d6e67918863d992bf79ed38bb69589824cc21bc1
shotaro kaneda <juanpablolvl99@gmail.com>
2019-07-23 00:58:13 -0300

    atualizando exportmanager do calculo de custo

--- mcp/mcp-jcf/src/main/java/ifpb/gpes/jcf/io/ChangeDifficultExportManager.java
+++ mcp/mcp-jcf/src/main/java/ifpb/gpes/jcf/io/ChangeDifficultExportManager.java
@@ -3,6 +3,7 @@ package ifpb.gpes.jcf.io;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import ifpb.gpes.Call;
 import ifpb.gpes.ExportManager;
 import ifpb.gpes.filter.FilterClassType;
@@ -19,37 +20,54 @@ public class ChangeDifficultExportManager extends ExportManager {
 
     @Override
     public void export(List<Call> elements) {
-        Predicate<Call> predicate = new FilterClassType("java.util.Map").or(new FilterClassType("java.util.Collection"));
-        List<Call> calls = elements.stream().filter(predicate).collect(Collectors.toList());
-        calculateFactors(calls, "java.util.List");
+        JsonNode categoriesNode = new JsonFile(getClass().getClassLoader().getResourceAsStream("categories.json")).toJsonObject();
+        //
+        Arrays.asList("java.util.List", "java.util.Map", "java.util.Set").forEach(interfaceFullName -> {
+            Predicate<Call> predicate = new FilterClassType(interfaceFullName);
+            List<Call> calls = elements.stream().filter(predicate).collect(Collectors.toList());
+            Map<String, Set<String>> categoriesMethodsMap = classifyCallsByCategories(calls, categoriesNode);
+            // get interface simple name
+            String[] split = interfaceFullName.split("\\.");
+            String interfaceSimpleName = split[split.length-1];
+            //
+            ArrayNode nodes = calculateChangeMetricByCategoriesAndInterface(categoriesMethodsMap, categoriesNode, interfaceSimpleName);
+            //
+            System.out.println(nodes);
+            System.out.println("-------");
+        });
     }
 
-    private void calculateFactors(List<Call> calls, String interfaceFullName) {
-        ObjectMapper mapper = new ObjectMapper();
-        String[] splitedInterfaceName = interfaceFullName.split("\\.");
-        String interfaceSimpleName = splitedInterfaceName[splitedInterfaceName.length-1];
-        JsonNode categoriesNode = new JsonFile(getClass().getClassLoader().getResourceAsStream("categories.json")).toJsonObject();
-        ArrayNode unionAndIntersectionArray = (ArrayNode) new JsonFile(getClass().getClassLoader().getResourceAsStream("union-intersection.json")).toJsonObject();
+    public Map<String, Set<String>> classifyCallsByCategories(List<Call> calls, JsonNode categoriesNode) {
         Map<String, Set<String>> categoriesMethodsMap = new HashMap<>();
-        List<Call> filteredCalls = calls.stream().filter(new FilterClassType(interfaceFullName)).collect(Collectors.toList());
+        // load the json categorization used to indentify the category of an method
         //
         categoriesNode.fields().forEachRemaining(entry -> {
-            Set<String> methodsByCategory = filteredCalls.stream().filter(call -> {
-                List<String> methods = mapper.convertValue(entry.getValue(), List.class);
-                Iterator<JsonNode> iterator = entry.getValue().iterator();
-                //
+            // send to a set all the methods that match with the given category
+            Set<String> methodsByCategory = calls.stream()
+            .filter(call -> {
+                List<String> methods = new ObjectMapper().convertValue(entry.getValue(), List.class);
+                // split method call name to remove the parameters of the comparation
                 return methods.contains(call.getMethodName().split("\\[")[0]);
-            }).map(call -> call.getMethodName().split("\\[")[0]).collect(HashSet::new, (set, methodName) -> {
-                set.add(methodName);
-            }, (set, other) -> set.addAll(other));
+            })
+            .map(call -> call.getMethodName().split("\\[")[0])
+            .collect(HashSet::new, (set, methodName) -> set.add(methodName), (set, other) -> set.addAll(other));
             categoriesMethodsMap.put(entry.getKey(), methodsByCategory);
         });
+        return categoriesMethodsMap;
+    }
+
+    private ArrayNode calculateChangeMetricByCategoriesAndInterface(Map<String, Set<String>> categoriesMethodsMap,
+            JsonNode categoriesNode, String interfaceSimpleName) {
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode resultArray = mapper.createArrayNode();
+        ArrayNode unionAndIntersectionArray = (ArrayNode) new JsonFile(getClass().getClassLoader().getResourceAsStream("union-intersection.json")).toJsonObject();
         //
         unionAndIntersectionArray.forEach(node ->  {
             boolean hasInterface = false;
+            // the compared interfaces in json file that has the respective union and intersection set of methods for the interfaces
             List<String> interfaces = mapper.convertValue(node.get("interfaces"), List.class);
             String otherInterface = "";
-            //
+            // getting the interfaces used in comparation
             for (String intf : interfaces) {
                 if (intf.equals(interfaceSimpleName)) {
                     hasInterface = true;
@@ -66,19 +84,31 @@ public class ChangeDifficultExportManager extends ExportManager {
                 Iterator<String> iterator = categoriesNode.fieldNames();
                 while (iterator.hasNext()) {
                     String category = iterator.next();
+                    // if the categories has no methods, is ignored
+                    if (categoriesMethodsMap.get(category).size() == 0)
+                        continue;
+                    //
                     List<String> intersectionList = mapper.convertValue(intersectionCategories.get(category), List.class);
-                    long intersection = categoriesMethodsMap.get(category).stream().filter(intersectionList::contains).count();
-                    long union = unionCategories.get(category).size();
-                    System.out.println("interfaces -> " + interfaceSimpleName + " to " + otherInterface);
-                    System.out.println("category -> " + category);
-                    System.out.println("metric -> " + calculateMetric(intersection, union));
-                    System.out.println("-------------");
+                    // verify if the methods in the category are present in intersection set
+                    double intersection = categoriesMethodsMap.get(category).stream().filter(intersectionList::contains).count();
+                    // get union quantity of methods for the metric calculation
+                    double union = unionCategories.get(category).size();
+                    /*
+                    if the quantity of intersection methods found are equal to the quantity of categorie methods
+                    are no dificult to change between the interfaces for this category
+                     */
+                    double factor = intersection/union;
+                    double metric = intersection == categoriesMethodsMap.get(category).size() ? 0 : 1/(1 + factor);
+                    // mounting result category object
+                    ObjectNode resultNode = mapper.createObjectNode();
+                    resultNode.set("interfaces", mapper.valueToTree(Arrays.asList(interfaceSimpleName, otherInterface)));
+                    resultNode.put("category", category);
+                    resultNode.put("metric", metric);
+                    //
+                    resultArray.add(resultNode);
                 }
             }
         });
-    }
-
-    public double calculateMetric(long intersection, long union) {
-        return 1 / (1 + (intersection/union));
+        return resultArray;
     }
 }
--- mcp/mcp-samples/src/main/java/ifpb/gpes/domain/SampleObject.java
+++ mcp/mcp-samples/src/main/java/ifpb/gpes/domain/SampleObject.java
@@ -9,6 +9,7 @@ public class SampleObject {
     private HasJCFObject a = new HasJCFObject();
     public void teste() {
         a.getElements().add(new HasJCFObject());
+        a.getElements().set(0, null);
     }
 
     public HasJCFObject m1() {
